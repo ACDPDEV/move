f9285e10385d2bf25036b4dd9ada809ed004571d
ACDPDEV <iamacdp.dev@gmail.com>
2025-05-22 11:58:27 -0500

    ⏫ Extraer el estado un nivel más arriba para filtrar los resultados

--- src/components/simulators/SearchFilter.tsx
+++ src/components/simulators/SearchFilter.tsx
@@ -1,10 +1,14 @@
 import { IconSearch } from "@tabler/icons-react";
 import { useState } from "react";
 
-function SearchFilter() {
-    const [ inputQuery, setInputQuery ] = useState("");
+function SearchFilter(
+    {getInput, setInput}: {
+        getInput: string;
+        setInput: React.Dispatch<React.SetStateAction<string>>;
+    }
+) {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputQuery(e.target.value);
+        setInput(e.target.value);
     };
     return (
         <form className="flex flex-row items-center gap-2 rounded-md bg-stone-900 focus-visible:ring-stone-800 focus-visible:border-stone-700 px-4 py-2">
@@ -13,7 +17,7 @@ function SearchFilter() {
                 placeholder="Buscar simuladores..."
                 name="search"
                 type="text"
-                value={inputQuery}
+                value={getInput}
                 onChange={handleChange}
                 className="border-0 rounded-md focus-visible:ring-0 focus-visible:border-0 bg-transparent w-full px-2 py-1 focus:outline-none"
             />
