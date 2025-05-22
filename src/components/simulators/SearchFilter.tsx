b6923805f8554d9125e5393df6c6ce375f5c0be0
ACDPDEV <iamacdp.dev@gmail.com>
2025-05-21 23:14:34 -0500

    ➕ Añadir el control del input de búsqueda

--- src/components/simulators/SearchFilter.tsx
+++ src/components/simulators/SearchFilter.tsx
@@ -1,15 +1,23 @@
-import { Input } from "@/components/ui/input";
 import { IconSearch } from "@tabler/icons-react";
+import { useState } from "react";
 
 function SearchFilter() {
+    const [ inputQuery, setInputQuery ] = useState("");
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputQuery(e.target.value);
+    };
     return (
-        <div className="flex flex-row items-center gap-2 rounded-md bg-stone-900 focus-visible:ring-stone-800 focus-visible:border-stone-700 px-4 py-2">
+        <form className="flex flex-row items-center gap-2 rounded-md bg-stone-900 focus-visible:ring-stone-800 focus-visible:border-stone-700 px-4 py-2">
             <IconSearch className="stroke-stone-400 hover:stroke-white hover:scale-110 transition-all" />
-            <Input
+            <input
                 placeholder="Buscar simuladores..."
-                className="border-0 rounded-md focus-visible:ring-0 focus-visible:border-0"
+                name="search"
+                type="text"
+                value={inputQuery}
+                onChange={handleChange}
+                className="border-0 rounded-md focus-visible:ring-0 focus-visible:border-0 bg-transparent w-full px-2 py-1 focus:outline-none"
             />
-        </div>
+        </form>
     )
 }
 
