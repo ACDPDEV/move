b7f4d3314ffb50da4ca44b0d069be1ccf99a8fa2
ACDPDEV <iamacdp.dev@gmail.com>
2025-05-22 11:59:16 -0500

    🔍 Añadir el filtrado por búsqueda en el GridResults

--- src/components/simulators/FilterForm.tsx
+++ src/components/simulators/FilterForm.tsx
@@ -1,13 +1,27 @@
 import { SearchFilter } from "@/components/simulators/SearchFilter";
 import { GridResults } from "@/components/simulators/GridResults";
 import { useSimulators } from "@/hooks/useSimulators";
+import { useState } from "react";
+import { type Simulator } from "@/types/Simulator";
 
 function FilterForm() {
+    const [ inputQuery, setInputQuery ] = useState("");
     const data = useSimulators();
+    const filterData = (data: Simulator[]) => {
+        return data.filter((simulator: Simulator) => {
+            return(
+                simulator.name.includes(inputQuery) ||
+                simulator.description.includes(inputQuery) ||
+                simulator.area.includes(inputQuery)
+            )
+        })
+    }
+    const filteredData = filterData(data);
+    
     return (
         <article className="grid grid-rows-[auto_1fr] w-full h-[calc(100vh-76px)] gap-4">
-            <SearchFilter />
-            <GridResults simulators={data} />
+            <SearchFilter getInput={inputQuery} setInput={setInputQuery} />
+            <GridResults simulators={filteredData} />
         </article>
     )
 }
