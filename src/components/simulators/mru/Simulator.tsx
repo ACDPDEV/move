cc5bf0cc6faa5e26c3b1cdaf35a49888861d0ba7
ACDPDEV <iamacdp.dev@gmail.com>
2025-06-14 17:54:01 +0000

    ➕ Actualizar el simulador de MRU

--- src/components/simulators/mru/Simulator.tsx
+++ src/components/simulators/mru/Simulator.tsx
@@ -1,13 +1,11 @@
 import { Canvas } from "@/components/simulators/Canvas";
-import { draw } from "@/components/simulators/mru/draw";
+import { preload, setup, loop } from "@/components/simulators/mru/draw";
 
 function MRUSimulator() {
     return (
-        <section class="flex flex-col w-full h-full gap-16 justify-center items-center">
-            <h1 class="text-4xl font-bold">MRU Simulator</h1>
-            <div class="h-full w-full">
-                <Canvas draw={draw} contextId="2d" resize={true} />
-            </div>
+        <section class="grid grid-cols-1 grid-rows-[auto_minmax(0,_1fr)] w-full min-h-full gap-8"> 
+            <h1 class="text-4xl font-bold text-white">MRU Simulator</h1>
+            <Canvas preload={preload} setup={setup} loop={loop}/>
         </section>
     )
 }
--- src/components/simulators/mru/draw.ts
+++ src/components/simulators/mru/draw.ts
@@ -1,16 +1,55 @@
-function draw(ctx: CanvasRenderingContext2D, frameCount: number) {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = '#fff'
-    ctx.beginPath()
-    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
-    ctx.fill()
-    ctx.closePath()
-    ctx.stroke()
-    ctx.fillStyle = '#fff'
-    ctx.font = '20px Arial'
-    ctx.textAlign = 'center'
-    ctx.fillText(`Frame Count: ${frameCount}`, ctx.canvas.width/2, ctx.canvas.height/2)
-    
-};
+import { Assets, Sprite, type Application } from 'pixi.js';
 
-export { draw };
+async function preload() {
+    const assets = [
+        { alias: 'background', src: 'https://pixijs.com/assets/tutorials/fish-pond/pond_background.jpg' },
+        { alias: 'fish1', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish1.png' },
+        { alias: 'fish2', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish2.png' },
+        { alias: 'fish3', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish3.png' },
+        { alias: 'fish4', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish4.png' },
+        { alias: 'fish5', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish5.png' },
+        { alias: 'overlay', src: 'https://pixijs.com/assets/tutorials/fish-pond/wave_overlay.png' },
+        { alias: 'displacement', src: 'https://pixijs.com/assets/tutorials/fish-pond/displacement_map.png' },
+    ];
+    await Assets.load(assets);
+}
+async function setup(app: Application, canvas: HTMLCanvasElement, container: HTMLDivElement) {
+    await app.init({
+        preference: "webgpu",
+        backgroundColor: "#333",
+        canvas: canvas,
+        resizeTo: container,
+    });
+};
+function loop(app: Application) {
+    // Create a background sprite.
+  const background = Sprite.from('background');
+
+  // Center background sprite anchor.
+  background.anchor.set(0.5);
+
+  /**
+   * If the preview is landscape, fill the width of the screen
+   * and apply horizontal scale to the vertical scale for a uniform fit.
+   */
+  if (app.screen.width > app.screen.height) {
+    background.width = app.screen.width * 1.2;
+    background.scale.y = background.scale.x;
+  } else {
+    /**
+     * If the preview is square or portrait, then fill the height of the screen instead
+     * and apply the scaling to the horizontal scale accordingly.
+     */
+    background.height = app.screen.height * 1.2;
+    background.scale.x = background.scale.y;
+  }
+
+  // Position the background sprite in the center of the stage.
+  background.x = app.screen.width / 2;
+  background.y = app.screen.height / 2;
+
+  // Add the background to the stage.
+  app.stage.addChild(background);
+}
+
+export { preload, setup, loop };
