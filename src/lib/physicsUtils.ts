0661008b4ff5620d725dac7785e7161413bbc29c
ACDPDEV <iamacdp.dev@gmail.com>
2025-06-17 05:23:36 +0000

    ➕ Barrel file

---
+++ src/lib/physicsUtils.ts
@@ -0,0 +1,11 @@
+import Vector2D from "./physics/classes/Vector2d";
+import Vector1D from "./physics/classes/Vector1d";
+
+/*
+    Esto es solo un barril c: 
+*/ 
+
+export {
+    Vector2D,
+    Vector1D,
+}
