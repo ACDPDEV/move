724186f707c78867273569e46bada47f710e9d06
ACDPDEV <iamacdp.dev@gmail.com>
2025-06-17 23:28:38 -0500

    ➕ Añadir el cálculo numerico de la clase Movil

--- src/components/simulators/mru/entities/Movil.ts
+++ src/components/simulators/mru/entities/Movil.ts
@@ -1,23 +1,27 @@
 import { Vector1D } from "@/lib/physicsUtils";
 
-interface MovilInterface {
-    position: Vector1D;
-    velocity: Vector1D;
-    acceleration: Vector1D;
-}
-
-class Movil implements MovilInterface {
+class Movil  {
     position: Vector1D;
     velocity: Vector1D;
     acceleration: Vector1D;
 
-    constructor(position: Vector1D, velocity: Vector1D, acceleration: Vector1D): MovilInterface {
+    constructor(position: Vector1D, velocity: Vector1D, acceleration: Vector1D) {
         this.position = position;
         this.velocity = velocity;
         this.acceleration = acceleration;
     }
 
+    update(deltaTime: number): void {
+        this.position.add(
+            this.velocity.add(
+                this.acceleration.copy().scale(deltaTime/60 /* pixel/frame a pixel/s */)
+            )
+        )
+    }
     
+    toString(): string {
+        return `Movil {position: ${this.position.x.toFixed(2)}, velocity: ${this.velocity.x.toFixed(2)}, acceleration: ${this.acceleration.x.toFixed(2)}}`
+    }
 }
 
-export { Movil, MovilInterface }
+export { Movil };
