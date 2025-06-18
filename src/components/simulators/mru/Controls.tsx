7f386003d6838b3cec4784ce75ee219a1e64f696
ACDPDEV <iamacdp.dev@gmail.com>
2025-06-17 23:27:58 -0500

    ➕ Añadir el componente de los botones

---
+++ src/components/simulators/mru/Controls.tsx
@@ -0,0 +1,11 @@
+function Controls() {
+    return (
+        <div class="flex flex-row gap-4 px-4 py-2 rounded-xl w-xl h-20 bg-stone-800 fixed b-0"> 
+            <button>Reset</button>
+            <button>Start</button>
+            <button>Stop</button>
+        </div>
+    )
+}
+
+export { Controls };
