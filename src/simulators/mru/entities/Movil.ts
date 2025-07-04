ed03f0d3cae5b3ad0d2738bb518abcab9b3307d4
ACDPDEV <iamacdp.dev@gmail.com>
2025-07-04 06:03:29 +0000

    🔧 Refactorizar la interfaz IMovilProps y modificar la estructura de la clase Movil

--- src/simulators/mru/entities/Movil.ts
+++ src/simulators/mru/entities/Movil.ts
@@ -1,12 +1,12 @@
 import { Vector2D } from "@/lib/physicsUtils";
 
-export interface IMovilProps {
-    id?: string;  // Make id optional
-    position: Vector2D;
-    velocity: Vector2D;
-    acceleration: Vector2D;
-    radius?: number;
-    color?: string;
+interface IMovilProps {
+    id?: string;
+    position: {x: number, y: number};
+    velocity: {x: number, y: number};
+    acceleration: {x: number, y: number};
+    radius: number;
+    color: string;
 }
 
 class Movil {
@@ -14,55 +14,35 @@ class Movil {
     position: Vector2D;
     velocity: Vector2D;
     acceleration: Vector2D;
+    radius: number;
+    color: string;
     private initialPosition: Vector2D;
     private initialVelocity: Vector2D;
     private initialAcceleration: Vector2D;
-    public _radius: number;  // Hacer público para permitir mutación
-    public _color: string;   // Hacer público para permitir mutación
-
-    // Getters públicos para acceder a las propiedades
-    get radius(): number {
-        return this._radius;
-    }
-
-    get color(): string {
-        return this._color;
-    }
-
-    // Setters públicos para permitir mutación
-    set radius(value: number) {
-        this._radius = value;
-    }
-
-    set color(value: string) {
-        this._color = value;
-    }
 
     constructor({ 
         id,
         position, 
         velocity, 
-        acceleration = new Vector2D(0, 0),
-        radius = 10, 
-        color = "#000000"
+        acceleration,
+        radius, 
+        color
     }: IMovilProps) {
         this.id = id;
-        this.position = position.copy();
-        this.velocity = velocity.copy();
-        this.acceleration = acceleration.copy();
-        this.initialPosition = position.copy();
-        this.initialVelocity = velocity.copy();
-        this.initialAcceleration = acceleration.copy();
-        this._radius = radius;
-        this._color = color;
+        this.position = new Vector2D(position.x, position.y);
+        this.velocity = new Vector2D(velocity.x, velocity.y);
+        this.acceleration = new Vector2D(acceleration.x, acceleration.y);
+        this.initialPosition = this.position.copy();
+        this.initialVelocity = this.velocity.copy();
+        this.initialAcceleration = this.acceleration.copy();
+        this.radius = radius;
+        this.color = color;
     }
 
     update(deltaTime: number): void {
-        // Apply acceleration to velocity
         this.velocity.x += this.acceleration.x * deltaTime / 60;
         this.velocity.y += this.acceleration.y * deltaTime / 60;
 
-        // Update position based on velocity
         this.position.x += this.velocity.x * deltaTime / 60;
         this.position.y += this.velocity.y * deltaTime / 60;
     }
@@ -76,40 +56,12 @@ class Movil {
     }
     
     
-    /**
-     * Reinicia el móvil a su posición y velocidad iniciales
-     */
     reset(): void {
         this.position = this.initialPosition.copy();
         this.velocity = this.initialVelocity.copy();
         this.acceleration = this.initialAcceleration.copy();
     }
 
-    /**
-     * Clean up resources when the entity is destroyed
-     */
-    destroy(): void {
-        // @ts-ignore - Permitir asignación undefined para cleanup
-        this.position = undefined;
-        // @ts-ignore
-        this.velocity = undefined;
-        // @ts-ignore
-        this.acceleration = undefined;
-        // @ts-ignore
-        this.initialPosition = undefined;
-        // @ts-ignore
-        this.initialVelocity = undefined;
-        // @ts-ignore
-        this.initialAcceleration = undefined;
-        // @ts-ignore
-        this._radius = undefined;
-        // @ts-ignore
-        this._color = undefined;
-    }
-
-    /**
-     * Obtiene una representación en cadena del móvil
-     */
     toString(): string {
         return `Movil {
             position: (${this.position.x.toFixed(2)}, ${this.position.y.toFixed(2)}), 
@@ -128,4 +80,5 @@ class Movil {
         `;
     }
 }
-export { Movil };
+
+export { Movil, type IMovilProps };
