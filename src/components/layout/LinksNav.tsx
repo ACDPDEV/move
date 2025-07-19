c009a2de24286017d3a9e8c1880cca48af17914b
ACDPDEV <iamacdp.dev@gmail.com>
2025-07-19 14:31:48 +0000

    🔧 Arreglar los condicionales de ruta

--- src/components/layout/LinksNav.tsx
+++ src/components/layout/LinksNav.tsx
@@ -12,12 +12,14 @@ function LinksNav() {
     const pathname = usePathname();
 
     return (
-        <ul className="flex flex-row w-fit h-fit p-1 gap-2 bg-[rgba(210,210,200,0.75)] dark:bg-[rgba(39,39,42,0.75)] backdrop-blur-md rounded-lg shadow-md shadow-[rgba(0,0,0,0.1)]">
-            <HomeLink isThisPage={pathname === '/' || pathname === ''} />
-            <SimulationsLink isThisPage={pathname === '/simulations/' || pathname === '/simulations'} />
-            <AboutLink isThisPage={pathname === '/about/' || pathname === '/about'} />
-            <ContactLink isThisPage={pathname === '/contact/' || pathname === '/contact'} />
-        </ul>
+        <>
+            <ul className="flex flex-row w-fit h-fit p-1 gap-2 bg-[rgba(210,210,200,0.75)] dark:bg-[rgba(39,39,42,0.75)] backdrop-blur-md rounded-lg shadow-md shadow-[rgba(0,0,0,0.1)]">
+                <HomeLink isThisPage={pathname === '/' || pathname === ''} />
+                <SimulationsLink isThisPage={pathname.includes('/simulations/') || pathname === '/simulations'} />
+                <AboutLink isThisPage={pathname.includes('/about/') || pathname === '/about'} />
+                <ContactLink isThisPage={pathname.includes('/contact/') || pathname === '/contact'} />
+            </ul>
+        </>
     );
 }
 
