3d95f821f058fa3b48a95340b8ad34c8519c3b7d
ACDPDEV <iamacdp.dev@gmail.com>
2025-07-21 19:08:28 -0500

    🔧 Quitar la UI principal de los docs

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,4 +1,3 @@
-import LayoutContainer from "@/components/layout/LayoutContainer";
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
@@ -7,9 +6,7 @@ import { baseOptions } from '@/app/layout.config';
 function Layout({ children }: { children: ReactNode }) {
   return (
     <DocsLayout tree={source.pageTree} {...baseOptions}>
-        <LayoutContainer>
-            {children}
-        </LayoutContainer>
+          {children}
     </DocsLayout>
   );
 }
