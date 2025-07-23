75d0a824f5f53cf18ea238e9094fae01132c9ba9
ADPDEV <iamacdp.dev@gmail.com>
2025-07-23 18:33:40 +0000

    🔧 Mostrar mensaje de error por la relación aspecto de la pantalla

--- src/simulations/cinematica/Simulation.tsx
+++ src/simulations/cinematica/Simulation.tsx
@@ -5,19 +5,32 @@ import FloatBar from './components/FloatBar';
 import TimeIndicators from './components/TimeIndicators';
 
 export default function CinematicaSimulation() {
-    console.log('render simulation');
     return (
-        <div className="relative w-full h-full bg-stone-900">
-            <Canvas style={{ width: '100%', height: '100%' }} />
+        <>
+            <div className="relative w-full h-full bg-stone-900 hidden md:flex">
+                <Canvas style={{ width: '100%', height: '100%' }} />
 
-            <Sidebar />
+                <Sidebar />
 
-            <TimeIndicators />
+                <TimeIndicators />
 
-            <div className="absolute flex flex-col bottom-0 left-1/2 translate-x-[-50%] gap-2 mb-10 justify-center items-center">
-                <FloatBar />
-                <TimeControls />
+                <div className="absolute flex flex-col bottom-0 left-1/2 translate-x-[-50%] gap-2 mb-10 justify-center items-center">
+                    <FloatBar />
+                    <TimeControls />
+                </div>
             </div>
-        </div>
+            <div className="relative w-screen h-screen bg-stone-900 flex md:hidden justify-center items-center p-4">
+                <div className="flex flex-col gap-4 text-center">
+                    <span className="prose dark:prose-invert">
+                        Esta simulación solo está disponible en tu tipo de
+                        pantalla, <strong>estamos trabajando en ello</strong>
+                    </span>
+                    <span className="prose dark:prose-invert">
+                        Sí estás en PC, cambia a pantalla completa y recarga la
+                        página
+                    </span>
+                </div>
+            </div>
+        </>
     );
 }
