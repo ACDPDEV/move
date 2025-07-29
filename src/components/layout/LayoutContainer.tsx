21e215ca4f83ebab359bc99db053ec81425ff790
ADPDEV <iamacdp.dev@gmail.com>
2025-07-29 20:54:08 +0000

    🔧 Arreglar el comportamiento del main container

--- src/components/layout/LayoutContainer.tsx
+++ src/components/layout/LayoutContainer.tsx
@@ -1,17 +1,21 @@
-import Header from "@/components/layout/Header";
-import { ScrollArea } from "../ui/scroll-area";
+import Header from '@/components/layout/Header';
+import { ScrollArea } from '../ui/scroll-area';
 
-function LayoutContainer({ children }: Readonly<{ 
+function LayoutContainer({
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <ScrollArea className="flex flex-col w-screen h-screen overflow-hidden p-2 rounded-md">
-            <Header /> 
-            <main className="w-full h-full mt-[52px]">
-                {children}
-            </main>
-        </ScrollArea>
-    )
+        <>
+            <Header />
+            <ScrollArea className="flex flex-col w-screen h-screen overflow-hidden p-2 rounded-md">
+                <main className="mt-14 rounded-md overflow-hidden">
+                    {children}
+                </main>
+            </ScrollArea>
+        </>
+    );
 }
 
 export default LayoutContainer;
