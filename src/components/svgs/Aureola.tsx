b05e9ce566928da67c5ae0021d79fdf88e66d923
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-01 04:07:16 +0000

    ➕ Añadir SVGs como componentes para el Hero

---
+++ src/components/svgs/Aureola.tsx
@@ -0,0 +1,58 @@
+function Aureola({
+    className,
+}: Readonly<{ className?: string }> & React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            width="2332"
+            height="1489"
+            viewBox="0 0 2332 1489"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+        >
+            <g filter="url(#filter0_f_46_38)">
+                <path
+                    d="M338.587 710.924C241.285 813.5 263.961 801.596 244 899.297C224.04 996.997 197.416 1071.08 295.836 1193.9C394.257 1316.73 371.575 1253.45 523.487 1190.62C675.398 1127.79 932.447 1176.82 1058.95 1158.86C1185.45 1140.89 1247.93 1053.95 1385.46 1037.29C1522.99 1020.63 1575.22 892.408 1709.83 811.681C1844.45 730.954 2021.4 759.194 2074.29 626.594C2127.18 493.994 2118.01 429.696 2031.54 312.822C1945.07 195.947 1806.99 228.433 1615.25 231.23C1423.5 234.026 1224.62 273.897 1079.25 391.128C933.881 508.358 940.019 575.952 789 667C637.981 758.048 435.89 608.347 338.587 710.924Z"
+                    fill="url(#paint0_linear_46_38)"
+                    fill-opacity="0.16"
+                />
+            </g>
+            <defs>
+                <filter
+                    id="filter0_f_46_38"
+                    x="0"
+                    y="0"
+                    width="2332"
+                    height="1489"
+                    filterUnits="userSpaceOnUse"
+                    color-interpolation-filters="sRGB"
+                >
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feBlend
+                        mode="normal"
+                        in="SourceGraphic"
+                        in2="BackgroundImageFix"
+                        result="shape"
+                    />
+                    <feGaussianBlur
+                        stdDeviation="112.5"
+                        result="effect1_foregroundBlur_46_38"
+                    />
+                </filter>
+                <linearGradient
+                    id="paint0_linear_46_38"
+                    x1="1914.27"
+                    y1="-327.426"
+                    x2="2999.98"
+                    y2="3469.02"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stop-color="#5D4EF1" />
+                    <stop offset="0.586538" stop-color="#56FF23" />
+                </linearGradient>
+            </defs>
+        </svg>
+    );
+}
+
+export default Aureola;
---
+++ src/components/svgs/Line.tsx
@@ -0,0 +1,37 @@
+function Line({
+    className,
+}: Readonly<{ className?: string }> & React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            width="1747"
+            height="1102"
+            viewBox="0 0 1747 1102"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+        >
+            <path
+                d="M2 1101C2 1101 81.8343 845.921 218.5 787.5C355.166 729.079 427.178 916.341 594 847C760.822 777.659 580.101 505.97 769.5 331C958.899 156.03 1388 423 1545 331C1702 239 1745 1 1745 1"
+                stroke="url(#paint0_linear_46_39)"
+                stroke-opacity="0.5"
+                stroke-width="3"
+            />
+            <defs>
+                <linearGradient
+                    id="paint0_linear_46_39"
+                    x1="1934.5"
+                    y1="-33.9999"
+                    x2="-238.5"
+                    y2="1077"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stop-color="#151618" stop-opacity="0" />
+                    <stop offset="0.480769" stop-color="#303236" />
+                    <stop offset="1" stop-color="#151618" stop-opacity="0" />
+                </linearGradient>
+            </defs>
+        </svg>
+    );
+}
+
+export default Line;
