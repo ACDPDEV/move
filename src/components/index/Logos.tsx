4c65bf473cb0073b3980f7cc74d2809b5dfd0fc7
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-03 02:24:48 +0000

    ➕ Añadir la sección de Logos

---
+++ src/components/index/Logos.tsx
@@ -0,0 +1,18 @@
+function Logos() {
+    return (
+        <div className="w-full h-40 border-x border-b border-[#222326] flex flex-row items-center justify-center py-8 gap-8 sm:gap-32">
+            <img
+                src="/sanjuan.avif"
+                alt="San Juan"
+                className="w-auto h-full saturate-50 sepia-25"
+            />
+            <img
+                src="/eureka.avif"
+                alt="Eureka"
+                className="w-auto h-full saturate-50 sepia-25"
+            />
+        </div>
+    );
+}
+
+export default Logos;
