501d7b2b62ef6873df4f515abf2de58bf828fff8
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-03 06:12:57 +0000

    ➕ Añadir el Footer

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -3,17 +3,19 @@ import Features from '@/components/index/Features';
 import Logos from '@/components/index/Logos';
 import About from '@/components/index/About';
 import Ready from '@/components/index/Ready';
+import Footer from '@/components/index/Footer';
 
 function Home() {
     return (
         <main className="flex flex-col w-full h-full justify-center items-center text-[#FCFCFD]">
-            <article className="w-full max-w-3xl h-full flex flex-col items-center justify-center">
+            <article className="relative w-full max-w-3xl h-full flex flex-col items-center justify-center">
                 <Hero />
                 <Features />
                 <Logos />
                 <About />
                 <Ready />
             </article>
+            <Footer />
         </main>
     );
 }
