378b1081cec0da3fcf7947506f0d2ceb4d379ccd
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-03 06:45:04 +0000

    ➕ Añadir SVGs como componentes para el Foote

---
+++ src/components/svgs/EBorder.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+function EBorder(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            width={216}
+            height={298}
+            viewBox="0 0 216 298"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="M214.5 289a8 8 0 01-8 8H9.5a8 8 0 01-8-8v-49.186a8 8 0 018-8h197a8 8 0 018 8V289zM190.69 166.611a8 8 0 01-8 8H9.5a8 8 0 01-8-8v-47.954a8 8 0 018-8h173.19a8 8 0 018 8v47.954zM205.06 58.186a8 8 0 01-8 8H9.5a8 8 0 01-8-8V9a8 8 0 018-8h187.56a8 8 0 018 8v49.186z"
+                stroke="CurrentColor"
+                strokeWidth={2}
+            />
+        </svg>
+    );
+}
+
+export default EBorder;
