9d37ddfb9a2c9e267ef63eb97304269c0ecbafdd
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-03 06:45:19 +0000

    ➕ Añadir al footer las letras MOVE

--- src/components/index/Footer.tsx
+++ src/components/index/Footer.tsx
@@ -1,12 +1,16 @@
 import { IconStereoGlasses } from '@tabler/icons-react';
 import Link from 'next/link';
+import MBorder from '../svgs/MBorder';
+import OBorder from '../svgs/OBorder';
+import VBorder from '../svgs/VBorder';
+import EBorder from '../svgs/EBorder';
 
 function Footer() {
     return (
-        <footer className="w-full bg-[#151618] py-12 border-t border-[#222326]">
+        <footer className="relative w-full overflow-hidden bg-[#151618] py-12 border-t border-[#222326]">
             <div className="container mx-auto px-6">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-                    <div className="col-span-1 md:col-span-2">
+                    <div className="col-span-1 sm:col-span-2">
                         <h3 className="text-2xl font-bold gradient-text mb-4 text-[#FCFCFD]">
                             Simuladores Educativos
                         </h3>
@@ -84,7 +88,7 @@ function Footer() {
                     </div>
                 </div>
 
-                <div className="border-t border-[#222326] mt-8 pt-8">
+                <div className="mt-8 pt-8">
                     <div className="flex flex-col md:flex-row justify-between items-center">
                         <div className="text-[#9A9CAC] text-sm mb-4 md:mb-0">
                             © 2025 Simuladores Educativos - Colegio San Juan.
@@ -101,6 +105,12 @@ function Footer() {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="absolute left-1/2 bottom-0 translate-y-1/2 -translate-x-1/2 w-full h-auto px-2 sm:px-12 pb-8 [&>*]:w-auto [&>*]:h-full [&>*]:text-[#222326] flex flex-row justify-center items-center space-x-2 opacity-50">
+                <MBorder />
+                <OBorder />
+                <VBorder />
+                <EBorder />
             </div>
         </footer>
     );
