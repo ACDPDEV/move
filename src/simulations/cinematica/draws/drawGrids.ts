b61085d4ec89b26ea86b325d419ff1ec8be0d26a
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-06 00:37:36 +0000

    🔧 Mejorar el gap, para que se escale

--- src/simulations/cinematica/draws/drawGrids.ts
+++ src/simulations/cinematica/draws/drawGrids.ts
@@ -1,5 +1,24 @@
 import { usePlaneStore } from '../stores/usePlaneStore';
 
+function gapFactor(number: number): number {
+    if (number === 0) return 0;
+    const stringFromNumber = number.toString();
+    const [intPart, decimalPart = ''] = stringFromNumber.split('.');
+    if (intPart !== '0') {
+        const digits = intPart.length;
+        return digits;
+    }
+    const match = decimalPart.match(/^0+/);
+    const count = match ? match[0].length : 0;
+    return count;
+}
+
+function getGap(length: number, scale: number): number {
+    const rel = length / scale;
+    const gap = Math.pow(10, gapFactor(rel) - 1) * scale;
+    return gap;
+}
+
 function drawGrids(ctx: CanvasRenderingContext2D, dark: boolean): void {
     const plane = usePlaneStore.getState();
 
@@ -8,7 +27,12 @@ function drawGrids(ctx: CanvasRenderingContext2D, dark: boolean): void {
         plane.position.x * plane.scale,
         plane.position.y * plane.scale * -1,
     ];
-    const gap = plane.gap * plane.scale;
+
+    // Separación en base diez, números de la forma 10**n
+    const gap =
+        width > height
+            ? getGap(height, plane.scale)
+            : getGap(width, plane.scale);
 
     // Prevenir bucles infinitos
     if (gap <= 0.1) {
