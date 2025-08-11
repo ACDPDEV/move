4a2eff7c29a4690d15376aae0437458ab46f4232
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-11 05:03:38 +0000

    🔧 Mover el manejo del tokenizado de la URL (no solo para el parámetro entities)

--- src/simulations/cinematica/utils/encodeAndDecodeEntities.ts
+++ src/simulations/cinematica/utils/encodeAndDecodeEntities.ts
@@ -64,29 +64,3 @@ export function decompressData(data: string): Entity[] {
         return [];
     }
 }
-
-const encodeCompact = (json: string) =>
-    json
-        .replace(/"/g, '~') // comillas → tilde
-        .replace(/:/g, '_') // dos puntos → guion bajo
-        .replace(/{/g, 'z') // llave izquierda → z
-        .replace(/}/g, 'Z') // llave derecha → Z
-        .replace(/\[/g, 'y') // corchete izquierdo → y
-        .replace(/\]/g, 'Y') // corchete derecho → Y
-        .replace(/,/g, 'x') // coma → x
-        .replace(/\./g, 'X') // coma → X
-        .replace(/#/g, 'w'); // # -> w
-
-const decodeCompact = (str: string) =>
-    str
-        .replace(/~/g, '"')
-        .replace(/_/g, ':')
-        .replace(/z/g, '{')
-        .replace(/Z/g, '}')
-        .replace(/y/g, '[')
-        .replace(/Y/g, ']')
-        .replace(/x/g, ',')
-        .replace(/X/g, '.')
-        .replace(/w/g, '#');
-
-export { encodeCompact, decodeCompact };
