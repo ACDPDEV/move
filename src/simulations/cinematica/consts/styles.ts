bd659b47905a7d40dc323b6630f6bfdd6be9b3a1
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-11 05:02:04 +0000

    🔧 Extraer los estilos de algunos componentes

---
+++ src/simulations/cinematica/consts/styles.ts
@@ -0,0 +1,7 @@
+const styles: Record<string, string> = {
+    button: 'w-auto h-9 bg-[#2B3B31] rounded-md flex flex-row p-2 gap-1 items-center justify-center cursor-pointer',
+    icon: 'text-[#89A996] w-5 h-5',
+    iconSecondary: 'text-[#567663] w-4 h-4',
+};
+
+export default styles;
