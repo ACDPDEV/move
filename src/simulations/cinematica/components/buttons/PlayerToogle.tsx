08797f7ee56b4556dce16052d697e5ae8d7b099f
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-11 04:59:14 +0000

    🔧 Mejorar la UI del componente player button

--- src/simulations/cinematica/components/buttons/PlayerToogle.tsx
+++ src/simulations/cinematica/components/buttons/PlayerToogle.tsx
@@ -1,55 +1,39 @@
-import { Button } from '@/components/ui/button';
+'use client';
 import { IconPlayerPause, IconPlayerPlay } from '@tabler/icons-react';
 import { useTimeStore } from '@/simulations/cinematica/stores/useTimeStore';
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipTrigger,
-} from '@/components/ui/tooltip';
-import { useSearchParams, useRouter, usePathname } from 'next/navigation';
-import {
-    compressData,
-    encodeCompact,
-} from '../../utils/encodeAndDecodeEntities';
+import { useURL } from '@/simulations/cinematica/hooks/useURL';
+import { compressData } from '../../utils/encodeAndDecodeEntities';
 import { useEntityStore } from '../../stores/useEntityStore';
+import Button from '../ui/button';
+import styles from '../../consts/styles';
 
-function PlayerToggle({ className }: { className?: string }) {
-    const searchParams = useSearchParams();
-    const pathname = usePathname();
-    const { replace } = useRouter();
+function usePlayerToggle() {
+    const { setURLParams } = useURL();
 
     const isPlaying = useTimeStore((s) => s.isPlaying);
     const togglePlayer = useTimeStore((s) => s.togglePlayer);
 
     const onClick = () => {
         togglePlayer();
-        const params = new URLSearchParams(searchParams);
-        params.set('t', useTimeStore.getState().time.toFixed(2));
-        params.set(
-            'd',
-            encodeCompact(compressData(useEntityStore.getState().entities)),
-        );
-        replace(`${pathname}?${params.toString()}`);
+        setURLParams({
+            t: useTimeStore.getState().time.toFixed(2),
+            d: compressData(useEntityStore.getState().entities),
+        });
     };
 
+    return { isPlaying, onClick };
+}
+
+function PlayerToggle() {
+    const { isPlaying, onClick } = usePlayerToggle();
     return (
-        <Tooltip>
-            <TooltipTrigger asChild>
-                <Button
-                    onClick={onClick}
-                    className={`text-stone-700 dark:text-stone-300  dark:hover:bg-stone-700 p-2 rounded transition-all duration-200 hover:scale-125 focus:outline-none focus:ring-2 focus:ring-stone-500${className}`}
-                    type="button"
-                    variant="outline"
-                >
-                    {isPlaying ? (
-                        <IconPlayerPause size={20} />
-                    ) : (
-                        <IconPlayerPlay size={20} />
-                    )}
-                </Button>
-            </TooltipTrigger>
-            <TooltipContent>{isPlaying ? 'Pausar' : 'Iniciar'}</TooltipContent>
-        </Tooltip>
+        <Button onClick={onClick} tooltip={isPlaying ? 'Pausar' : 'Iniciar'}>
+            {isPlaying ? (
+                <IconPlayerPause className={styles.icon} />
+            ) : (
+                <IconPlayerPlay className={styles.icon} />
+            )}
+        </Button>
     );
 }
 
