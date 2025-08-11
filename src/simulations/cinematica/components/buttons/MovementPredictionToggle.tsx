4f8d7493eb6b7797eace7df3295169320dc8101a
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-11 04:58:59 +0000

    🔧 Mejorar la UI del componente movement prediction

--- src/simulations/cinematica/components/buttons/MovementPredictionToggle.tsx
+++ src/simulations/cinematica/components/buttons/MovementPredictionToggle.tsx
@@ -1,38 +1,37 @@
-import { Button } from '@/components/ui/button';
 import { IconLink, IconLinkOff } from '@tabler/icons-react';
 import { useTimeStore } from '@/simulations/cinematica/stores/useTimeStore';
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipTrigger,
-} from '@/components/ui/tooltip';
+import Button from '../ui/button';
+import styles from '../../consts/styles';
 
-function MovementPredictionToggle() {
+function useMovementPredictionToggle() {
     const togglePrediction = useTimeStore((s) => s.togglePrediction);
     const movementPrediction = useTimeStore((s) => s.movementPrediction);
 
+    const onClick = () => {
+        togglePrediction();
+    };
+
+    return { onClick, movementPrediction };
+}
+
+function MovementPredictionToggle() {
+    const { onClick, movementPrediction } = useMovementPredictionToggle();
+
     return (
-        <Tooltip>
-            <TooltipTrigger asChild>
-                <Button
-                    onClick={() => togglePrediction()}
-                    variant="outline"
-                    className="text-stone-700 dark:text-stone-300  dark:hover:bg-stone-700 p-2 rounded transition-all duration-200 hover:scale-125 focus:outline-none focus:ring-2 focus:ring-stone-500"
-                    type="button"
-                >
-                    {movementPrediction ? (
-                        <IconLink size={20} />
-                    ) : (
-                        <IconLinkOff size={20} />
-                    )}
-                </Button>
-            </TooltipTrigger>
-            <TooltipContent>
-                {movementPrediction
+        <Button
+            onClick={onClick}
+            tooltip={
+                movementPrediction
                     ? 'Desactivar predicción'
-                    : 'Activar predicción'}
-            </TooltipContent>
-        </Tooltip>
+                    : 'Activar predicción'
+            }
+        >
+            {movementPrediction ? (
+                <IconLinkOff className={styles.icon} />
+            ) : (
+                <IconLink className={styles.icon} />
+            )}
+        </Button>
     );
 }
 
