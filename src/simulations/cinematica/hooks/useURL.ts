d092b1d1880c87d9653b971ce7e5a3f1edab7667
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-11 05:04:04 +0000

    🔧 Extraer el manejo dele estado de la URL en un custom hook (get y set)

---
+++ src/simulations/cinematica/hooks/useURL.ts
@@ -0,0 +1,26 @@
+'use client';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { decodeURLParam, encodeURLParam } from '../utils/URLToken';
+
+function useURL() {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    const setURLParams = (params: { [key: string]: string }) => {
+        const URLParams = new URLSearchParams(searchParams);
+        Object.entries(params).forEach(([key, value]) => {
+            URLParams.set(key, encodeURLParam(value));
+        });
+        replace(`${pathname}?${URLParams.toString()}`);
+    };
+
+    const getURLParams = (param: string) => {
+        if (!searchParams.get(param)) return null;
+        return decodeURLParam(searchParams.get(param)!);
+    };
+
+    return { setURLParams, getURLParams };
+}
+
+export { useURL };
