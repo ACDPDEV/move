15f08806f2ddbdecb2b307486b54bf4ed3f4cdc5
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-11 04:59:27 +0000

    🔧 Mejorar la UI del componente reset button

--- src/simulations/cinematica/components/buttons/ResetButton.tsx
+++ src/simulations/cinematica/components/buttons/ResetButton.tsx
@@ -1,46 +1,38 @@
-import { Button } from '@/components/ui/button';
+'use client';
 import { useTimeStore } from '../../stores/useTimeStore';
 import { IconReload } from '@tabler/icons-react';
 import { useEntityStore } from '../../stores/useEntityStore';
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipTrigger,
-} from '@/components/ui/tooltip';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useURL } from '../../hooks/useURL';
+import { compressData } from '../../utils/encodeAndDecodeEntities';
+import Button from '../ui/button';
+import styles from '../../consts/styles';
 
-function ResetButton() {
-    const searchParams = useSearchParams();
-    const pathname = usePathname();
-    const { replace } = useRouter();
-
+function useResetButton() {
     const reset = useTimeStore((s) => s.reset);
-
+    const { setURLParams } = useURL();
     const onClick = () => {
+        useEntityStore
+            .getState()
+            .updateAllEntities(-useTimeStore.getState().time);
         useEntityStore.getState().entities.forEach((entity) => {
-            entity.update(-useTimeStore.getState().time);
             entity.resetTrajectory();
         });
         reset();
-        const params = new URLSearchParams(searchParams);
-        params.set('t', '0');
-        replace(`${pathname}?${params.toString()}`);
+        setURLParams({
+            t: useTimeStore.getState().time.toFixed(2),
+            d: compressData(useEntityStore.getState().entities),
+        });
     };
+    return { onClick };
+}
+
+function ResetButton() {
+    const { onClick } = useResetButton();
 
     return (
-        <Tooltip>
-            <TooltipTrigger asChild>
-                <Button
-                    onClick={onClick}
-                    className="text-stone-700 dark:text-stone-300  dark:hover:bg-stone-700 p-2 rounded transition-all duration-200 hover:scale-125 focus:outline-none focus:ring-2 focus:ring-stone-500"
-                    type="button"
-                    variant="outline"
-                >
-                    <IconReload size={20} />
-                </Button>
-            </TooltipTrigger>
-            <TooltipContent>Reiniciar</TooltipContent>
-        </Tooltip>
+        <Button onClick={onClick} tooltip="Reiniciar">
+            <IconReload className={styles.icon} />
+        </Button>
     );
 }
 
