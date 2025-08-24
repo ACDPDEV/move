d363fcfac6f32b354479363ecab341d2fe1c928b
ACDPDEV <iamacdp.dev@gmail.com>
2025-08-24 02:32:02 +0000

    Pending changes exported from your codespace

--- src/simulations/cinematica/components/inputs/EntityInput.tsx
+++ src/simulations/cinematica/components/inputs/EntityInput.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useRef } from 'react';
+import { memo, useEffect, useRef } from 'react';
 import {
     useEntityStore,
     type EntityStore,
@@ -6,6 +6,7 @@ import {
 import { Entity, EntityProps } from '@/simulations/cinematica/entities/Entity';
 import { useURL } from '../../hooks/useURL';
 import { compressData } from '../../utils/encodeAndDecodeEntities';
+import Input from '../ui/input';
 
 interface EntityInputProps {
     className?: string;
@@ -17,7 +18,8 @@ interface EntityInputProps {
         | 'velocity.y'
         | 'acceleration.x'
         | 'acceleration.y'
-        | 'radius';
+        | 'radius'
+        | 'color';
     setError: React.Dispatch<React.SetStateAction<string>>;
 }
 
@@ -29,7 +31,7 @@ const EntityInput = memo(function EntityInput({
 }: EntityInputProps) {
     const inputRef = useRef<HTMLInputElement>(null);
     const previousRef = useRef<number>(0);
-
+    
     // Suscripción a TODO el estado, filtrar la propiedad que interesa
     useEffect(() => {
         // Función de actualización interna
@@ -56,27 +58,27 @@ const EntityInput = memo(function EntityInput({
 
         // Subscribe devuelve la función de unsubscribe
         const unsubscribe = useEntityStore.subscribe(subscriber);
-
+        
         // Inicializa el valor
         const initEntity = useEntityStore
-            .getState()
-            .entities.find((e) => e.id === entityId);
+        .getState()
+        .entities.find((e) => e.id === entityId);
         const propsArray = entityProp.split('.');
         const initValue = initEntity
-            ? (initEntity[propsArray[0] as keyof EntityProps][
-                  propsArray[1] as keyof EntityProps[keyof EntityProps]
-              ] as number)
-            : 0;
+        ? (initEntity[propsArray[0] as keyof EntityProps][
+            propsArray[1] as keyof EntityProps[keyof EntityProps]
+        ] as number)
+        : 0;
         if (inputRef.current && initEntity) {
             if (initValue) {
                 inputRef.current.value = initValue.toFixed(2);
             }
             previousRef.current = initValue;
         }
-
+        
         return unsubscribe;
     }, [entityId]);
-
+    
     // Actualizar store cuando el usuario modifica
     const updateX = useEntityStore((s) => s.updateSpecificPropOfEntity);
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -87,8 +89,8 @@ const EntityInput = memo(function EntityInput({
             return;
         }
         const cleaned = e.target.value
-            .replace(/^0+(?=\d)/, '')
-            .replace(/^0+$/, '');
+        .replace(/^0+(?=\d)/, '')
+        .replace(/^0+$/, '');
         e.target.value = cleaned;
         previousRef.current = n;
         updateX(entityId, entityProp, n);
@@ -100,28 +102,31 @@ const EntityInput = memo(function EntityInput({
             d: compressData(useEntityStore.getState().entities),
         });
     };
-    const onSubmit = (e: React.InputEvent<HTMLInputElement>) => {
+    const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         e.preventDefault();
         setURLParams({
             d: compressData(useEntityStore.getState().entities),
         });
     };
+    
+    const vectorInput = (
+        <Input
+            ref={inputRef}
+            name="time"
+            type='text'
+            onChange={onChange}
+            onBlur={onBlur}
+            onKeyDown={onKeyDown}
+            placeholder="0"
+        />
+    )
 
     return (
         <div className="w-full h-9 bg-[#2B3B31] rounded-md flex flex-1 flex-row p-2 gap-1 items-center justify-between cursor-pointer">
             <span className="text-[#89A996] w-3 h-5 text-sm font-mono">
                 {entityProp.includes('.x') ? 'x' : 'y'}
             </span>
-            <input
-                type="text"
-                name="time"
-                ref={inputRef}
-                onChange={onChange}
-                onBlur={onBlur}
-                onSubmit={onSubmit}
-                placeholder="0"
-                className="flex grow w-full h-5 bg-transparet p-0 text-start text-sm text-[#D3DFD8] font-mono focus:outline-none"
-            />
+            {vectorInput}
         </div>
     );
 });
