f168843b19902967d5e6dcf122cb3e6507d8b5f1
ACDPDEV <iamacdp.dev@gmail.com>
2025-09-11 15:35:22 +0000

    ➕ Añadir más opciones prefefinidas

--- src/simulations/cinematica/components/PredefinedVariables.tsx
+++ src/simulations/cinematica/components/PredefinedVariables.tsx
@@ -6,9 +6,49 @@ import { useVariablesStore } from "../stores/useVariablesStore";
 
 const predefinedVariables = [
     {
-        name: 'gravedad',
+        name: 'Gravedad de la Tierra',
         type: 'acceleration',
         value: { x: 0, y: -9.81 },
+    },
+    {
+        name: 'Gravedad de Mercurio',
+        type: 'acceleration',
+        value: { x: 0, y: -3.7 },
+    },
+    {
+        name: 'Gravedad de Venus',
+        type: 'acceleration',
+        value: { x: 0, y: -8.87 },
+    },
+    {
+        name: 'Gravedad de Marte',
+        type: 'acceleration',
+        value: { x: 0, y: -3.71 },
+    },
+    {
+        name: 'Gravedad de Júpiter',
+        type: 'acceleration',
+        value: { x: 0, y: -24.79 },
+    },
+    {
+        name: 'Gravedad de Saturno',
+        type: 'acceleration',
+        value: { x: 0, y: -10.44 },
+    },
+    {
+        name: 'Gravedad de Urano',
+        type: 'acceleration',
+        value: { x: 0, y: -8.69 },
+    },
+    {
+        name: 'Gravedad de Neptuno',
+        type: 'acceleration',
+        value: { x: 0, y: -11.15 },
+    },
+    {
+        name: 'Gravedad de la Luna',
+        type: 'acceleration',
+        value: { x: 0, y: -1.62 },
     },
 ];
 
@@ -27,7 +67,7 @@ function PredefinedVariables() {
                 </Button>
             </PopoverTrigger>
             <PopoverContent  className="p-0" asChild>
-                <div className="bg-[#202C25] w-16 h-fit p-2 gap-2 border-0">
+                <div className="flex flex-col bg-[#202C25] w-16 h-fit max-h-72 overflow-y-scroll p-2 gap-2 border-0">
                 {
                         predefinedVariables.map((variable) => { 
                         const disabled = variables.find((v) => v.name === variable.name) ? true : false;
@@ -41,7 +81,7 @@ function PredefinedVariables() {
                         >
                             <span className="flex flex-row gap-3 items-center">
                                 <IconArrowDownRightCircle className={styles.icon} />
-                                <span className={disabled ? styles.suffix : styles.preffix}>
+                                <span className={disabled ? 'text-[#567663] font-mono text-sm' : 'text-[#89A996] font-mono text-sm'}>
                                     {variable.name}
                                 </span>
                             </span>
