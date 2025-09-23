97df222af5a78cf3f83499659d6abfd1932b4ea0
ACDPDEV <iamacdp.dev@gmail.com>
2025-09-23 00:09:15 +0000

    ➕ Agregar el estilo de un botón destructivo

--- src/simulations/cinematica/consts/styles.ts
+++ src/simulations/cinematica/consts/styles.ts
@@ -1,5 +1,8 @@
 const styles: Record<string, string> = {
-    button: 'w-auto h-9 bg-[#2B3B31] rounded-md flex flex-row p-2 gap-1 items-center justify-center cursor-pointer',
+    defaultButton:
+        'w-auto h-9 bg-[#2B3B31] rounded-md flex flex-row p-2 gap-1 items-center justify-center cursor-pointer',
+    destructiveButton:
+        'w-auto h-9 bg-[#712828] rounded-md flex flex-row p-2 gap-1 items-center justify-center cursor-pointer',
     icon: 'text-[#89A996] w-5 h-5',
     iconSecondary: 'text-[#567663] w-4 h-4',
     input: 'w-32 h-9 bg-[#2B3B31] rounded-md flex flex-row p-2 gap-1 items-center justify-between text-sm text-[#D3DFD8] font-mono cursor-pointer',
